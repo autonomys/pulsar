9099b26f255eabb7b89fcd7d09b17d46ac56848a
ozgunozerk <ozgun@subspace.network>
2022-12-06 10:56:51 +0000

    Deploying to gh-pages from  @ 2ce02e87f96ccc1487f9608bcc88859fb49c259b 🚀

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"subspace_cli":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"subspace_cli/config/enum.ChainConfig.html\" title=\"enum subspace_cli::config::ChainConfig\">ChainConfig</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- subspace_cli/commands/init/sidebar-items.js
+++ subspace_cli/commands/init/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["DEFAULT_CHAIN",""],["DEFAULT_PLOT_SIZE","defaults for the user config file"]],"fn":[["get_config_from_user_inputs","gets the necessary information from user, and writes them to the given configuration file"],["init","implementation of the `init` command"]]};
+window.SIDEBAR_ITEMS = {"fn":[["get_config_from_user_inputs","gets the necessary information from user, and writes them to the given configuration file"],["init","implementation of the `init` command"]]};
