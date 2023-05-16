183aa327b479be3c164e1a95454cb66466c9bed1
Özgün Özerk <ozgun@subspace.network>
2023-05-16 16:05:18 +0300

    key generation if user does not have a public key beforehand (#200)
    * key generation if user does not have a public key beforehand
    
    * zeroize sensitive data

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,8 +1,14 @@
-use std::io::Write;
+use std::io::{BufRead, Write};
 use std::str::FromStr;
 
-use color_eyre::eyre::{Context, Result};
+use color_eyre::eyre::{eyre, Context, Error, Result};
+use crossterm::terminal::{Clear, ClearType};
+use crossterm::{cursor, execute};
+use rand::prelude::IteratorRandom;
+use sp_core::Pair;
 use strum::IntoEnumIterator;
+use subspace_sdk::PublicKey;
+use zeroize::Zeroizing;
 
 use crate::config::{
     create_config, AdvancedFarmerSettings, AdvancedNodeSettings, ChainConfig, Config, FarmerConfig,
@@ -11,7 +17,7 @@ use crate::config::{
 use crate::utils::{
     directory_parser, get_user_input, node_directory_getter, node_name_parser,
     plot_directory_getter, print_ascii_art, print_run_executable_command, print_version,
-    reward_address_parser, size_parser,
+    reward_address_parser, size_parser, yes_or_no_parser,
 };
 
 /// implementation of the `init` command
@@ -40,10 +46,15 @@ pub(crate) fn init() -> Result<()> {
 /// gets the necessary information from user, and writes them to the given
 /// configuration file
 fn get_config_from_user_inputs() -> Result<Config> {
-    // GET USER INPUTS...
-    // get reward address
-    let reward_address =
-        get_user_input("Enter your farmer/reward address: ", None, reward_address_parser)?;
+    // check if user has an existing reward address
+    let reward_address_exist = get_user_input(
+        "Do you have an existing farmer/reward address? [y/n]: ",
+        None,
+        yes_or_no_parser,
+    )?;
+
+    let reward_address = generate_or_get_reward_address(reward_address_exist)
+        .context("reward address creation failed")?;
 
     // get node name
     let default_node_name = whoami::username();
@@ -112,4 +123,78 @@ fn get_config_from_user_inputs() -> Result<Config> {
     };
 
     Ok(Config { farmer: farmer_config, node: node_config, chain })
+}
+
+fn generate_or_get_reward_address(reward_address_exist: bool) -> Result<PublicKey> {
+    if reward_address_exist {
+        return get_user_input("Enter your farmer/reward address: ", None, reward_address_parser);
+    }
+
+    let wants_new_key = get_user_input(
+        "Do you want to create a new farmer/reward key? [y/n]: ",
+        None,
+        yes_or_no_parser,
+    )?;
+
+    if !wants_new_key {
+        return Err(eyre!("New key creation was not confirmed"));
+    }
+
+    // generate new mnemonic and key pair
+    let (pair, phrase, seed): (
+        sp_core::sr25519::Pair,
+        String,
+        <sp_core::sr25519::Pair as Pair>::Seed,
+    ) = Pair::generate_with_phrase(None);
+    let _seed = Zeroizing::new(seed);
+    let phrase = Zeroizing::new(phrase);
+    let words: Vec<&str> = phrase.split_whitespace().collect();
+
+    println!(
+        "IMPORTANT NOTICE: The mnemonic displayed below is crucial to regain access to your \
+         account in case you forget your credentials. It's highly recommended to store it in a \
+         secure and retrievable location. Failure to do so may result in permanent loss of access \
+         to your account.\n"
+    );
+    println!(
+        "Please press 'Enter' after you've securely stored the mnemonic. Once you press 'Enter', \
+         the mnemonic will no longer be visible in this interface for security reasons.\n"
+    );
+    // saving position, since we will later clear the mnemonic
+    println!("Here is your mnemonic:");
+    execute!(std::io::stdout(), cursor::SavePosition).context("save position failed")?;
+    println!("{}", phrase.as_str());
+    std::io::stdin().lock().lines().next();
+
+    // clear the mnemonic
+    execute!(std::io::stdout(), cursor::RestorePosition).context("restore cursor failed")?;
+    execute!(std::io::stdout(), Clear(ClearType::FromCursorDown))
+        .context("clear mnemonic failed")?;
+
+    println!("...redacted...");
+
+    // User has to provide 3 randomly selected words from the mnemonic
+    let mut rng = rand::thread_rng();
+    let word_indexes: Vec<usize> = (0..words.len()).choose_multiple(&mut rng, 3);
+
+    for index in &word_indexes {
+        loop {
+            let word = get_user_input(
+                &format!("Enter the {}th word in the mnemonic: ", index + 1),
+                None,
+                |input| Ok::<String, Error>(input.to_owned()),
+            )?;
+
+            if word == words[*index] {
+                break;
+            } else {
+                println!("incorrect word, please try again.")
+            }
+        }
+    }
+
+    // print the public key and return it
+    println!("Your new public key is: {}", pair.public());
+    let public_key_array = pair.public().0;
+    Ok(public_key_array.into())
 }
