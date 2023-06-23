9f411f470021fc71722f7db98a5bba88b884ba10
ozgunozerk <ozgun@subspace.network>
2023-06-23 05:30:40 +0000

    Deploying to gh-pages from  @ f8e66c0d00e70bdd238fbe77c44019109d682c1b 🚀

--- implementors/strum/trait.IntoEnumIterator.js
+++ implementors/strum/trait.IntoEnumIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"subspace_cli":[["impl IntoEnumIterator for <a class=\"enum\" href=\"subspace_cli/enum.Commands.html\" title=\"enum subspace_cli::Commands\">Commands</a>"],["impl IntoEnumIterator for <a class=\"enum\" href=\"subspace_cli/config/enum.ChainConfig.html\" title=\"enum subspace_cli::config::ChainConfig\">ChainConfig</a>"]]
+"subspace_cli":[["impl IntoEnumIterator for <a class=\"enum\" href=\"subspace_cli/config/enum.ChainConfig.html\" title=\"enum subspace_cli::config::ChainConfig\">ChainConfig</a>"],["impl IntoEnumIterator for <a class=\"enum\" href=\"subspace_cli/enum.Commands.html\" title=\"enum subspace_cli::Commands\">Commands</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
