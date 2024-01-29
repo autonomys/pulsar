426c0e9705d455b50c1076e522f76e8bc21e4dd5
Parth Desai <desaiparth08@gmail.com>
2024-01-29 13:35:11 +0530

    address review feedback

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let output = std::process::Command::new("git").args(["rev-parse", "HEAD"]).output().unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
-    println!("cargo:rustc-env=GIT_HASH={git_hash}");
-}
