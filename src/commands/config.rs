313b7bfa6a6ed55f1f0152b166c1d479e664d848
abhi3700 <dalvath3700@gmail.com>
2024-01-31 01:34:09 +0530

    changed the logic from 'set one param only' to 'either or all params'

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -1,9 +1,12 @@
-//! Config CLI command of pulsar is about setting the parameters:
+//! Config CLI command of pulsar is about setting either or all of the
+//! parameters:
 //! - chain
 //! - farm size
 //! - reward address
 //! - node directory
 //! - farm directory
+//!
+//! and showing the set config details.
 
 use std::fs;
 
@@ -42,59 +45,54 @@ pub(crate) async fn config(
             config_path.to_str().expect("Expected stringified config path")
         );
     } else {
-        match chain {
-            Some(c) => {
-                // TODO: update (optional) the chain
+        // no options provided
+        if chain.is_none()
+            && farm_size.is_none()
+            && reward_address.is_none()
+            && node_dir.is_none()
+            && farm_dir.is_none()
+        {
+            println!("At least one option has to be provided.\nTry `pulsar config -h`");
+            return Ok(());
+        }
+
+        if let Some(_c) = chain {
+            // TODO: update (optional) the chain
+        }
+
+        if let Some(ref f) = farm_size {
+            // update (optional) the farm size
+            let farm_size = size_parser(&f)?;
+            // if let Ok(farm_size) = size_parser(&farm_size.unwrap()) {}
+            if farm_size >= MIN_FARM_SIZE {
+                config.farmer.farm_size = farm_size;
+            } else {
+                bail!("Farm size must be ≥ 2 GB");
             }
-            None => match farm_size {
-                Some(f) => {
-                    // update (optional) the farm size
-                    let farm_size = size_parser(&f)?;
-                    // if let Ok(farm_size) = size_parser(&farm_size.unwrap()) {}
-                    if farm_size >= MIN_FARM_SIZE {
-                        config.farmer.farm_size = farm_size;
-                    } else {
-                        bail!("Farm size must be ≥ 2 GB");
-                    }
-                }
-                None => match reward_address {
-                    Some(r) => {
-                        // update (optional) the reward address
-                        let reward_address = reward_address_parser(&r)?;
-                        config.farmer.reward_address = reward_address;
-                    }
-                    None => match node_dir {
-                        Some(n) => {
-                            // update (optional) the node directory
-                            let node_dir = dir_parser(&n).expect("Invalid node directory");
-                            create_and_move_data(config.node.directory.clone(), node_dir.clone())
-                                .expect("Error in setting new node directory.");
-                            config.node.directory = node_dir;
-                        }
-                        None => match farm_dir {
-                            Some(fd) => {
-                                // update (optional) the farm directory
-                                let farm_dir = dir_parser(&fd).expect("Invalid farm directory");
-                                create_and_move_data(
-                                    config.farmer.farm_directory.clone(),
-                                    farm_dir.clone(),
-                                )
-                                .expect("Error in setting new farm directory.");
-                                if farm_dir.exists() {
-                                    config.farmer.farm_directory = farm_dir;
-                                }
-                            }
-                            None => {
-                                println!(
-                                    "At least one option has to be provided.\nTry `pulsar config \
-                                     -h`"
-                                );
-                                return Ok(());
-                            }
-                        },
-                    },
-                },
-            },
+        }
+
+        if let Some(ref r) = reward_address {
+            // update (optional) the reward address
+            let reward_address = reward_address_parser(&r)?;
+            config.farmer.reward_address = reward_address;
+        }
+
+        if let Some(ref n) = node_dir {
+            // update (optional) the node directory
+            let node_dir = dir_parser(&n).expect("Invalid node directory");
+            create_and_move_data(config.node.directory.clone(), node_dir.clone())
+                .expect("Error in setting new node directory.");
+            config.node.directory = node_dir;
+        }
+
+        if let Some(ref fd) = farm_dir {
+            // update (optional) the farm directory
+            let farm_dir = dir_parser(&fd).expect("Invalid farm directory");
+            create_and_move_data(config.farmer.farm_directory.clone(), farm_dir.clone())
+                .expect("Error in setting new farm directory.");
+            if farm_dir.exists() {
+                config.farmer.farm_directory = farm_dir;
+            }
         }
 
         // Save the updated configuration back to the file
